0c5404c70581e9207e0f33718fa15faa20d13d8e
Lyova98 <lyovayesayan1@gmail.com>
2022-06-28 15:49:47 +0400

    add todo object in response

--- src/resolvers/todo/updateTodo.js
+++ src/resolvers/todo/updateTodo.js
@@ -4,26 +4,30 @@ export default {
   Mutation: {
     updateTodo: async (parent, args) => {
       try {
-        await Todo.update(
-          {
+        const todo = await Todo.findByPk(args.id);
+        if (todo && todo.id) {
+          await todo.update({
             name: args.name,
             status: args.status,
-          },
-          {
-            where: {
-              id: args.id,
-            },
-          }
-        );
-        return {
-          message: 'Todo updated successfully!',
-          status: 200,
-        };
+          });
+          return {
+            message: 'Todo updated successfully!',
+            status: 200,
+            todo: todo,
+          };
+        } else {
+          return {
+            message: 'Todo not found!',
+            status: 400,
+            todo: null,
+          };
+        }
       } catch (e) {
         console.log('e', e);
         return {
           message: 'Update todo failed!',
           status: 400,
+          todo: null,
         };
       }
     },
